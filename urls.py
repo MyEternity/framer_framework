0e94629f8763ae91e8c99da7260cc21ad4e10297
MyEternity <spddarkforce47@gmail.com>
2022-08-27 23:57:35 +0300

    l4

--- urls.py
+++ urls.py
@@ -1,8 +1,13 @@
-from views import Main, Info, Contacts, About
+from datetime import date
+
+from views import Main, Info, Contacts, About, StudyPrograms, CoursesList, CreateCourse, CreateCategory, CategoryList, \
+    CopyCourse
 
 
 def data(request):
     request['data'] = {}
+    request['objects'] = {}
+    request['date'] = date.today()
 
 
 def key(request):
@@ -16,4 +21,10 @@ routes = {
     '/info/': Info(),
     '/contacts/': Contacts(),
     '/about/': About(),
+    '/study_programs/': StudyPrograms(),
+    '/courses-list/': CoursesList(),
+    '/create-course/': CreateCourse(),
+    '/create-category/': CreateCategory(),
+    '/category-list/': CategoryList(),
+    '/copy-course/': CopyCourse()
 }
